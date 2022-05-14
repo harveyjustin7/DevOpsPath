aa826dc69c6c47345998d8a81e50ac75401ae48f
ZimmeLu <zimmelu4@students.zhaw.ch>
2022-05-14 10:13:43 +0200

    changed FizzBuzzConverter

--- backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverterTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverterTest.java
@@ -17,8 +17,8 @@ public class FizzBuzzConverterTest {
     public void fizzBuzzConvertor2() {
 
         FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
-        Assert.assertEquals("2", fizzBuzz.convert(2));
-
+        Assert.assertEquals("2", fizzBuzz.convert(3));
+// change back to 2
     }
 
     @Test
